50fda32fc13e1448cce93071a57fffcdca32c70c
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2022-07-04 11:45:41 +0200

    Erstellen der Funktion Create_File_with_random_numbers

--- utils.c
+++ utils.c
@@ -1,19 +1,42 @@
 #include "utils.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
-extern void array_print(int data[], size_t n)
+
+void array_print(int data[], size_t n)
+{
+    for (int i=0; i < n; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+int create_file_with_random_numbers(char *filename, size_t n)
+{
+    FILE *file_ptr = fopen(filename, "w");
+    if (file_ptr == NULL) 
+    {
+        fprintf(stderr, "Can't create file: %s !\n", filename);
+        return -1;
+    }
+
+    srand(time(0));
+ 
+    for(int i = 0; i<n; i++)
+    {
+        printf("%d\n", rand() % 1000);
+    }
+
+ 
+    fclose(file_ptr);
+
+}
+int delete_file(char *filename)
 {
 
 }
-extern int create_file_with_random_numbers(char *filename, size_t n)
+int *load_data_from(char *filename, size_t n)
 {
 
-}
-extern int delete_file(char *filename)
-{
-
-}
-extern int *load_data_from(char *filename, size_t n)
-{
-    
 }
