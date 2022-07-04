d5836be8a55cddb5f27e008e6a6dff03b23f3a79
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2022-07-04 11:20:22 +0200

    Erstellen des Headerfiles für die Funktionen

---
+++ sortalgs.h
@@ -0,0 +1,10 @@
+#ifndef _SORTALGS_H_
+#define _SORTALGS_H_
+
+extern void array_bubble_sort(int data[], int n);
+extern void array_selection_sort(int data[], int n);
+extern void array_insertion_sort(int data[], int n);
+extern void array_merge_sort(int data[], int n);
+extern void array_quick_sort(int data[], int n);
+
+#endif /* _SORTALGS_H_ */
