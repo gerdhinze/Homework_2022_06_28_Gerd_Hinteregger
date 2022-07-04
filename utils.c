2a6c74ae7ad3b8dd82214cce4ad695ef4f88c9f4
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2022-07-04 11:32:45 +0200

    Modifizieren von utils.c

---
+++ utils.c
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+
+extern void array_print(int data[], size_t n)
+{
+
+}
+extern int create_file_with_random_numbers(char *filename, size_t n)
+{
+
+}
+extern int delete_file(char *filename)
+{
+
+}
+extern int *load_data_from(char *filename, size_t n)
+{
+    
+}
--- utils.h
+++ utils.h
@@ -4,10 +4,10 @@
 
 extern void array_print(int data[], size_t n);
 //Gibt ein Array gegebener länge auf der Konsole aus.
-extern int create_file_with_random_numbers(char *filename, size_t n)
+extern int create_file_with_random_numbers(char *filename, size_t n);
 //Erstellt ein Textfile mit n Zufallszahlen [0,10000), eine Zahl pro Zeile.
-extern int delete_file(char *filename)
+extern int delete_file(char *filename);
 //Löscht ein File aus dem Filesystem (siehe stdio.h remove())
-extern int *load_data_from(char *filename, size_t n)
+extern int *load_data_from(char *filename, size_t n);
 
 #endif /* _UTILS_H_ */
